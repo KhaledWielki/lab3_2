2cf0ded32219f4c34de00d22979d34998e3f6f0c
Adam Rezner <195047@edu.p.lodz.pl>
2017-03-27 22:30:26 +0200

    Required imports to NewsLoaderUnitTests added

--- src/test/java/edu/iis/mto/staticmock/NewsLoaderUnitTests.java
+++ src/test/java/edu/iis/mto/staticmock/NewsLoaderUnitTests.java
@@ -3,6 +3,13 @@ package edu.iis.mto.staticmock;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import static org.powermock.api.mockito.PowerMockito.*;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
 
 public class NewsLoaderUnitTests {
 
